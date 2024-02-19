f77a27fff13affc3d5827be05053aaf7753bdfbf
Tino Hager <tino.hager@nager.at>
2024-02-19 13:35:44 +0100

    Optimize logic

--- src/Nager.WindowsCalendarWeek/Nager.WindowsCalendarWeek/Program.cs
+++ src/Nager.WindowsCalendarWeek/Nager.WindowsCalendarWeek/Program.cs
@@ -7,33 +7,57 @@ namespace Nager.WindowsCalendarWeek
 {
     internal static class Program
     {
+        private static Timer RefreshIconTimer;
+        private static NotifyIcon CalendarWeekNotifyIcon;
+        private static int OneDayMilliseconds = 86400000;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            //TODO: Add logic to detect day change and redraw icon
+            var firstRefreshInterval = (int)(OneDayMilliseconds - Math.Floor(DateTime.Now.TimeOfDay.TotalMilliseconds));
+
+            RefreshIconTimer = new Timer();
+            RefreshIconTimer.Interval = firstRefreshInterval;
+            RefreshIconTimer.Tick += new EventHandler(RefreshIconTimer_Tick);
+            RefreshIconTimer.Start();
 
             var bmp = GetBitmapOfCurrentCalendarWeek();
 
-            var trayIcon = new NotifyIcon
+            CalendarWeekNotifyIcon = new NotifyIcon
             {
                 Icon = Icon.FromHandle(bmp.GetHicon()),
                 ContextMenuStrip = new ContextMenuStrip
                 {
                     Items =
                     {
-                        new ToolStripMenuItem("Online Kalender", null, new EventHandler(DetailItemClickEvent), "Online Kalendar"),
-                        new ToolStripMenuItem("Quit", null, new EventHandler(QuitItemClickEvent), "Quit")
+                        new ToolStripMenuItem("Online Kalender", null, new EventHandler(OnlineCalendarItemClickEvent), "ToolStripMenuItem_OnlineCalendar"),
+                        new ToolStripMenuItem("Feiertage", null, new EventHandler(HolidayItemClickEvent), "ToolStripMenuItem_Holiday"),
+                        new ToolStripMenuItem("Quit", null, new EventHandler(QuitItemClickEvent), "ToolStripMenuItem_Quit")
                     }
                 },
                 Visible = true
             };
 
-            trayIcon.MouseClick += TrayIcon_Click;
+            CalendarWeekNotifyIcon.MouseClick += TrayIcon_Click;
 
             Application.Run();
+        }
+
+        private static void RefreshIconTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshIconTimer.Interval = OneDayMilliseconds;
+
+            var oldIcon = CalendarWeekNotifyIcon.Icon;
+            if (oldIcon != null)
+            {
+                oldIcon.Dispose();
+            }
+
+            var bmp = GetBitmapOfCurrentCalendarWeek();
+            CalendarWeekNotifyIcon.Icon = Icon.FromHandle(bmp.GetHicon());
         }
 
         private static void TrayIcon_Click(object sender, MouseEventArgs e)
@@ -44,16 +68,28 @@ namespace Nager.WindowsCalendarWeek
             }
         }
 
-        private static void DetailItemClickEvent(object sender, EventArgs e)
+        private static void OnlineCalendarItemClickEvent(object sender, EventArgs e)
         {
             OpenOnlineCalendar();
         }
 
+        private static void HolidayItemClickEvent(object sender, EventArgs e)
+        {
+            OpenHoliday();
+        }
+
         private static void OpenOnlineCalendar()
         {
+            // show first or second half of the year
             var ext = DateTime.Today.Month > 6 ? "-2" : "";
 
             var target = $"https://kalenderwoche.at/kalender/{DateTime.Today.Year}{ext}";
+            System.Diagnostics.Process.Start(target);
+        }
+
+        private static void OpenHoliday()
+        {
+            var target = $"https://date.nager.at/";
             System.Diagnostics.Process.Start(target);
         }
 
